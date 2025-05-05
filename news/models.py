3a8b468dd9a688307b58be67a97f5e81d99a45fe
QOSHANTAY <nuratdinovsalamat1@gmail.com>
2025-05-05 14:51:08 +0500

    news page:not ready

---
+++ news/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.category_name
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    news = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
