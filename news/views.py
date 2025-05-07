76ff421ea58a567a8b5700fc0afdaf8858a4ff35
QOSHANTAY <nuratdinovsalamat1@gmail.com>
2025-05-07 15:31:27 +0500

    there was added a category function

--- news/views.py
+++ news/views.py
@@ -3,9 +3,15 @@ from .models import Category,News
 
 def homepage(request):
     posts = News.objects.all()
-    return render(request,'home.html',{'ex':posts})
+    categories = Category.objects.all()
+    return render(request,'home.html',{'ex':posts,'categories':categories})
 
 
 def detail(request,id):
     news = get_object_or_404(News,id = id)
     return render(request,'detail.html',{'detail':news})
+
+def category_func(request,id):
+    categories = Category.objects.all()
+    news = News.objects.filter(category_id = id)
+    return render(request,'category.html',{'news':news,'categories':categories})
